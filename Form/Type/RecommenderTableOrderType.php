924521a0a495c529e61cce34df5c402af3675419
Unknown <peter.osvath@d-code.hu>
2019-05-24 15:55:51 +0200

    r

--- Form/Type/RecommenderTableOrderType.php
+++ Form/Type/RecommenderTableOrderType.php
@@ -108,13 +108,13 @@ class RecommenderTableOrderType extends AbstractType
                 $form = $event->getForm();
                 $column = $event->getData();
                 
-                    
-                $this->setupAvailableFunctionChoices(
-                    $form,
-                    $column
-                );
-            }
-        );
+
+                    $this->setupAvailableFunctionChoices(
+                        $form->getParent(),
+                        $column
+                    );
+                }
+            );
         
 
         
