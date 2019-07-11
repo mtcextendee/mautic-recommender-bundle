999b08594e3c09d0886542ee6d74c66d5c75d25c
Unknown <peter.osvath@d-code.hu>
2019-07-11 18:12:49 +0200

    standard mautic maintenance on recommender_event_log

--- Config/config.php
+++ Config/config.php
@@ -62,6 +62,12 @@ return [
                     'mautic.helper.integration'
                 ],
             ],
+            'mautic.recommender.maintenance.subscriber' => [
+                'class'     => 'MauticPlugin\MauticRecommenderBundle\EventListener\MaintenanceSubscriber',
+                'arguments' => [
+                    'doctrine.dbal.default_connection',
+                ],
+            ],
         ],
         'models'       => [
             'mautic.recommender.model.recommender' => [
